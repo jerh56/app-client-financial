2aa625a295cffbb217dd268f9e394d0997adee9e
Juan Ramos <eramos@coppel.com>
2022-04-04 18:09:51 -0600

    login func

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
+const url = require('url');
+const querystring = require('querystring');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+ /* Maneja la aplicación principal */
+router.get('/principal',
+     function(req, res){
+
+      let username = req.query.username;
+      let tickets = req.query.tickets;
+
+     console.log(username);
+     console.log(tickets);
+
+      //console.log(req.user);
+      res.render('principal', {message: 'Bienvenido', username: username, tickets:tickets});
 });
 
 module.exports = router;
